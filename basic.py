9b571c54d36f6dd0557361bfcfbd7061964c4da7
juanandresrod0519 <juan_andres_rodriguez_0519@hotmail.com>
2021-03-06 21:09:13 -0500

    2021-03-06 21:09

---
+++ basic.py
@@ -0,0 +1,121 @@
+import argparse
+
+def main():
+    args = get_args()
+    word = args.word
+    print("Hello-Basic")
+    print(word)
+    #1
+    my_dogs = 2
+    print(type(my_dogs))
+    my_dogs = ["dog-1","dog-2"]
+    print(type(my_dogs))
+    my_dogs = 30.1
+    print(type(my_dogs))
+    #2
+    word = 'narwhal'
+    print(word[0])  #--> 'n'
+    print('narwhal'[0]) #--> 'n'
+    print(word[6]) # --> 'l'
+    print(word[-1]) # --> 'l'
+    #3 [start:stop]
+    print(word[:3]) #--> 'nar'
+    print(word[3:]) #--> 'whal'
+    alphabet= 'abcdefghijklmnopqrstuvwxyz'
+    print(alphabet[2:])
+    print(alphabet[:4])
+    print(alphabet[3:6])
+    print(alphabet[::3])
+    print(alphabet[2:7:2])
+    #4
+    print(word.upper())
+    print(word.isupper())
+    print(word.upper().isupper())
+    print(len('narwhal'))
+    #5
+    char = word[0].lower()
+    print(char)
+    print(char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u')
+    #6
+    print('a' in 'aeiou')
+    print('k' in 'aeiou')
+    print("hello \nworld \tJA")
+    #7 
+    #String inmutability
+    name = "Juan Andres"
+    #name[0] = "p" ----> error
+    name = "j" + name[1:]
+    print(name)
+    letter = 'A'
+    print(letter * 23)
+
+    s="Hello World"
+    print(s.upper())
+    words = s.split(' ')
+    print(words)
+
+    #String formatting
+    print('Hola {} {}!'.format('Juan','Andres'))
+    print('Hola {r} {f}!'.format(f='Felipe',r='Rod'))
+    res = 100/777
+    print('R:{}'.format(res))
+    print('R:{r:1.3f}'.format(r=res))
+    print('R:{r:10.3f}'.format(r=res))
+    name = "Juan Andres"
+    print(f'Hello {name}')
+
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Juan Andres",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('word', metavar='word', help='A word')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    main()
+
+
+# word = 'narwhal'
+# word[0]  --> 'n'
+# 'narwhal'[0] --> 'n'
+# word[6] --> 'l'
+# word[-1] --> 'l'
+# [start:stop] 
+# word[:3] --> 'nar'
+# word[3:] --> 'whal'
+# word.upper()
+# word.isupper() --> F
+# word.upper().isupper() --> T
+# len('narwhal') --> 7
+
+# word = 'octopus'
+# char = word[0] --> 'o'
+
+# type(char) --> <class 'str'>
+
+# char == 'a' --> F
+# char == 'o' --> T
+# word = "hola" --> nothing ; word == "hola" --> T or F
+
+# word = 'OCTOPUS'
+# char = word[0]
+# char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' --> F
+# char = word[0].lower()
+# char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' --> F
+
+# 'a' in 'aeiou' --> T;  'b' in 'aeiou' --> F
+
+#article = ''
+#if word[0].lower() in 'aeiou':
+#article = 'an'
+#else:
+#article = 'a'
+
+#'Ahoy, Captain, {} {} off the larboard bow!'.format(article, word)
+#f'Ahoy, Captain, {article} {word} off the larboard bow!'
+
+#Positional:parser.add_argument('word', metavar='word', help='Word')
+
+#word[0] in 'aeiouAEIOU'
---
+++ list-practice.py
@@ -0,0 +1,155 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description='List Practice',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+    #1
+    my_list11 = []
+    my_list12 = [1,2,3]
+    my_list13 = [1,'hello',3,4]
+    my_list14 = ['hello',[1,2,4],['X']]
+    
+    #2
+    my_list21 = ['H','E','L','L','O']
+    print(my_list21[0])
+    n_list22 = ["Hello",[1,2,3,4]]
+    print(n_list22[0][1])
+    print(n_list22[1][3])
+
+    #3
+    my_list31 = ['H','E','L','L','O']
+    print(my_list31[-1])
+
+    #4
+    my_list41 = ['H','E','L','L','O','w','o','r','l','d']
+    print(my_list41[2:5])
+    print(my_list41[:-6])
+    print(my_list41[5:])
+    print(my_list41[:])
+
+    #5
+    print("5")
+    odd5 = [2, 4, 6, 8]
+    odd5[0] = 1
+    print(odd5)
+    odd5[1:4] = [3, 5, 7] # --> [1,3,5,7]
+    #odd5[1:3] = [3, 5, 7] # --> [1,3,5,7,8]
+    print(odd5)
+
+
+    #6
+    print("6")
+    odd6 = [1,3,5]
+    odd6.append(7)
+    print(odd6)
+    odd6.extend([9,11,13])
+    print(odd6)
+
+    #7
+    odd7 = [1,3,5]
+    print(odd7 + [7,9,11])
+    print(['re']*7)
+
+    #8
+    print("#8")
+    odd8 = [1,9]
+    odd8.insert(1,3)
+    print(odd8)
+    odd8[2:2]=[5,7]
+    print(odd8)
+
+    #9
+    print("#9")
+    my_list9 = ['H','E','L','L','O']
+    del my_list9[2]
+    print(my_list9)
+    del my_list9[1:5]
+    print(my_list9)
+
+    #10
+    my_list10 = ['p','r','o','b','l','e','m']
+    my_list10.remove('p')
+    print(my_list10)
+    print(my_list10.pop(0)) #--> 'r'
+    print(my_list10) #--> ['o', 'b', 'l', 'e', 'm']
+    my_list10.clear()
+    print(my_list10)
+
+    #11
+    print('#11')
+    my_list11 = ['p','r','o','b','l','e','m']
+    my_list11[2:3] = []
+    print(my_list11)
+
+    #12
+    my_list12 = [3, 8, 1, 6, 0, 8, 4]
+    print(my_list12.index(8)) # --> 1
+    print(my_list12.count(8)) # --> 2
+    my_list12.sort()
+    print(my_list12) # ->[0, 1, 3, 4, 6, 8, 8]
+    my_list12.reverse()
+    print(my_list12) # ->[8, 8, 6, 4, 3, 1, 0]
+
+    #13 LIST COMPREHENSION
+    pow2 = [2 ** x for x in range(10)]
+    print(pow2) #--> [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+    pow2 = []
+    for x in range(10):
+        pow2.append(2 ** x)
+    
+    #14
+    print("#14")
+    #pow2 = [2 ** x for x in range(10) if x > 5]
+    pow2 = [2**x for x in range(10) if x > 5]
+    print(pow2) #--> [64, 128, 256, 512]
+    odd = [x for x in range(20) if x % 2 == 1] #--> [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    print(odd)
+    [x+y for x in ['Python ','C '] for y in ['Language','Programming']] #--> ['Python Language', 'Python Programming', 'C Language', 'C Programming']
+
+    #15
+    my_list15 = ['p', 'r', 'o', 'b', 'l', 'e', 'm']
+    print('p' in my_list15) #-->T
+    for fruit in ['apple','banana','mango']:
+        print("I like",fruit)
+
+    #16
+    h_letters = [ letter for letter in 'human' ]
+    print( h_letters)
+
+    #lambda
+    letters = list(map(lambda x: x, 'human'))
+    print(letters)
+
+    #17
+    number_list = [ x for x in range(20) if x % 2 == 0]
+    print(number_list)
+
+    #18
+    num_list = [y for y in range(100) if y % 2 == 0 if y % 5 == 0]
+    print(num_list)
+
+    #if..else
+    obj = ["Even" if i%2==0 else "Odd" for i in range(10)]
+    print(obj)
+
+    #19
+    transposed = []
+    matrix = [[1, 2, 3, 4], [4, 5, 6, 8]]
+    for i in range(len(matrix[0])):
+        transposed_row = []
+        for row in matrix:
+            transposed_row.append(row[i])
+        transposed.append(transposed_row)
+    print(transposed)
+    print()
+
+    matrix2 = [[1, 2], [3,4], [5,6], [7,8]]
+    l = [[row[i] for row in matrix2] for i in range(2)]
+    print(l)
+
+if __name__ == '__main__':
+    main()
